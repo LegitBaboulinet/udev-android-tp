e4ed6d4781ef1b88b6ae4e07a3306f54dcddb551
GhostMonk3y <chartrer.mickael@gmail.com>
2019-01-23 09:48:07 +0100

    Ajout fonctionnalité SMS / Mail

--- app/src/main/java/com/example/udevAndroidTp/activities/ClientActivity.java
+++ app/src/main/java/com/example/udevAndroidTp/activities/ClientActivity.java
@@ -1,8 +1,13 @@
 package com.example.udevAndroidTp.activities;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.udevAndroidTp.R;
 import com.example.udevAndroidTp.classes.Client;
@@ -42,7 +47,7 @@ public class ClientActivity extends AppCompatActivity {
         On récupére la valeur envoyé par l'intent (Voir ListPersonneActivity.java)
          */
         //Client ClientByIntent = (Client) getIntent().getExtras().getSerializable("clientActivityIntent");
-        Client ClientByIntent = new Client(
+        final Client ClientByIntent = new Client(
                 "Monsieur",
                 "Jean-Claude",
                 "Van Damme",
@@ -62,6 +67,35 @@ public class ClientActivity extends AppCompatActivity {
         phoneNumberTextView.setText(ClientByIntent.getPhoneNumber());
         mailTextView.setText(ClientByIntent.getMail());
 
+        phoneNumberTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + ClientByIntent.getPhoneNumber()));
+                smsIntent.putExtra("sms_body", "Test");
+                try {
+                    startActivity(smsIntent);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(phoneNumberTextView.getContext(), "Erreur lors de la création du SMS", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
+        mailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailIntent = new Intent(Intent.ACTION_SEND);
+                mailIntent.setType("message/rfc822");
+                mailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{ClientByIntent.getMail()});
+                mailIntent.putExtra(Intent.EXTRA_SUBJECT, "Objet");
+                mailIntent.putExtra(Intent.EXTRA_TEXT, "Contenu du mail");
+
+                try {
+                    startActivity(Intent.createChooser(mailIntent, "Envoi d'un mail"));
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(mailTextView.getContext(), "Aucun client mail installé", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
     }
 }
